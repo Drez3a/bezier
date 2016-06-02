48e86f86107c75f6ea8334608fd2db9c1bc6d0e2
Drez3 <drezfabiola@gmail.com>
2016-06-02 16:56:07 -0300

    Update convex_hull.js

--- scripts/convex_hull.js
+++ scripts/convex_hull.js
@@ -1,84 +1,79 @@
-var giroEsq = false;
-
 function convexHull (points) { 
-/* encontrar quais são os índices do ponto superior esquerdo:
-   não precisa, porque se houverem dois(ou mais) pontos de menor grau com o eixo x (ou seja, eles formam
-   o mesmo angulo), então, eles pertencem a mesma reta e, consequentemente, ao poligono convexo.*/
-//var start = 0,
-//    end = points.length,
-//   pointsHull = points;
 
-//	quickSort(pointsHull, start, end);
+   /* pontos testados:
+   points = [{x: 40, y: 40}, {x: 300, y: 50}, {x: 225, y: 125}, {x: 350, y: 250}, {x:
+			100, y: 150}],
+   */
+   
+var ind = 0,
+		position = [],
+		ancor = points[0];
+		hull = [];
 
-}
-
-// https://pt.wikipedia.org/wiki/Exame_de_Graham
-// calcula o produto vetorial para determinar se o o giro foi a esquerda ou direita
+	for (var i=0; i<points.length; i++){
+		position[i] = {
+			angle: Math.atan2(points[i].x, points[i].y),
+			point: points[i]
+		}
+		
+		if(ancor.y > points[i].y) {
+			ind = i;
+			ancor = points[i];
+		}
+	}
+		
+	position.sort(function(prev, next) {
+	  return prev.angle - next.angle; 
+	});
+	
+	hull.push(ancor);
+	hull.push(position[0].point);
+//	position.push(ancor); // pra fechar o ciclo -DUVIDA: next(ABAIXO) DÁ INDEFINIDO QUANDO FAÇO ISSO ????????????????????
+		
+	position.splice(ind, 1); // retira ancor
+	
+	var prev = hull[0],
+		topo = hull[1],
+		next,
+		giroEsq = false,
+		x1, x2, x3, y1, y2, y3,
+		j = 2;
+	
+	// ainda vou ver a quantidade de loops
+	// ainda vou ver a logica ...
+	// DUVIDA: O PRODUTO VETORIAL ESTÁ DANDO NEGATIVO PARA PONTOS QUE EU SEI Q SERIA POSITIVO??????????????????
+	for (var i=1; i<position.length; i++) {		
+		next = position[i].point;
+				
+		x1 = prev.x;
+		x2 = topo.x;
+		x3 = next.x;
+		y1 = prev.y;
+		y2 = topo.y;
+		y3 = next.y;
+		
+		prodVetorial(x1, x2, x3, y1, y2, y3);
+		
+		if(giroEsq) {
+			//o indice de hull é sempre um a mais que position:
+			// hull[i+1] = position[i].point;
+			hull.push(next);	//ainda vou ver quando tiro ou boto em hull	
+			prev = hull[j-1];
+			topo = hull[j];
+			j++;
+		} else {
+			topo = next;
+		}
+					
+		console.log(giroEsq);
+	}
+		
+	// calcula o produto vetorial para determinar se o o giro foi a esquerda ou direita
 function prodVetorial( x1, x2, x3, y1, y2, y3) {
-    var prod;
-	prod = (x2-x1)*(y3-y1)-(y2-y1)*(x3-x1);
+	var prod;
+    prod = (x2-x1)*(y3-y1)-(y2-y1)*(x3-x1);
+	console.log(prod);
     if (prod > 0){
 		giroEsq = true;
 	}	
  }
-
-// *** SORT FUNCTIONS ************************************************************
-// Ordena pelo valor do ângulo formado entre o ponto e o eixo X.
-function quickSort(pointsHull, start, end) {
-	if (start < end) {
-		var pivotPosition = order(pointsHull, start, end);
-        quickSort(pointsHull, start, pivotPosition - 1);
-        quickSort(pointsHull, pivotPosition + 1, end);
-	}
-}
-
-function order(pointsHull, start, end) {
-	var i = start + 1, f = end;
-	var pivot = pointsHull[start];	
-	var angle_pivot = Math.atan2(pivot.x, pivot.y);
-	
-	while (i <= f) {
-	var angle_i = Math.atan2(pointsHull[i].x, pointsHull[i].y);
-	var angle_f = Math.atan2(pointsHull[f].x, pointsHull[f].y);
-
-		if (angle_i <= angle_pivot ) {
-			i++;
-		}
-		else if (angle_pivot < angle_f){
-			f--;
-		}
-		else {
-			var temp = pointsHull[i];
-			pointsHull[i] = pointsHull[f];
-			pointsHull[f] = temp;
-			i++;
-			f--;
-		}
-	}
-
-	pointsHull[start] = pointsHull[f];
-	pointsHull[f] = pivot;
-	return f;
-}
-
-/*** TESTE NO MAIN ***************************************************
-
-//verifica os angulos
-	for (var i=0; i < 5; i++){	
-		console.log(Math.atan2(points[i].x, points[i].y)); 
-	}
-	
-	console.log("passei");
-	
-	var myArray = points.slice(0), // para não mexer na memoria
-		start = 0,
-		end = myArray.length-1;
-
-	quickSort(myArray, start, end);
-	
-	for (var i=0; i < 5; i++){	
-		console.log(Math.atan2(myArray[i].x, myArray[i].y)) // >>> OK, ORDENOU!!!; 
-		// RESOLVIDO: MAS, por que ele tá ordenando a array points também???:
-		// console.log(Math.atan2(points[i].x, points[i].y)); 
-		}
-*/
