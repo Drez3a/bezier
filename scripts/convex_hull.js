d33c86e6aa2688478417c1394ff341ea08a57236
Drez3 <drezfabiola@gmail.com>
2016-06-03 02:36:55 -0300

    Update convex_hull.js
    1-Dúvida nos valores dos angulos permanece
    
    2-Fiz uma gambiarra pra testar se a logica tava certa:
    
    //erro
    	/* era pra retirar ancor e colocar no final	-------------------------------------------------------
    	-DUVIDA: ele insere porque o tamanho aumenta, mas o objeto fica indefinido,
    	 AFETANDO next(ABAIXO).
    	*/
    	position.push(ancor); // pra fechar o ciclo 	
    	position.splice(ind, 1); 
    
    //gambiarra
    	// isso é porque não estou conseguindo inserir "position.push(ancor); // pra fechar o ciclo"
    		if (i!=4){
    			next = position[i].point; // a conta seria apenas essa
    		} else {
    			next = points[0];
    		}

--- scripts/convex_hull.js
+++ scripts/convex_hull.js
@@ -1,17 +1,11 @@
-function convexHull (points) { 
-
-   /* pontos testados:
-   points = [{x: 40, y: 40}, {x: 300, y: 50}, {x: 225, y: 125}, {x: 350, y: 250}, {x:
-			100, y: 150}],
-   */
-   
-var ind = 0,
+	var ind = 0,
 		position = [],
 		ancor = points[0];
 		hull = [];
 
 	for (var i=0; i<points.length; i++){
 		position[i] = {
+			// os valores dos angulos estão estranhos
 			angle: Math.atan2(points[i].x, points[i].y),
 			point: points[i]
 		}
@@ -22,58 +16,77 @@ var ind = 0,
 		}
 	}
 		
-	position.sort(function(prev, next) {
+	/*position.sort(function(prev, next) {
 	  return prev.angle - next.angle; 
-	});
+	});*/
 	
-	hull.push(ancor);
-	hull.push(position[0].point);
-//	position.push(ancor); // pra fechar o ciclo -DUVIDA: next(ABAIXO) DÁ INDEFINIDO QUANDO FAÇO ISSO ????????????????????
+	hull.push(ancor); // j = 0
 		
-	position.splice(ind, 1); // retira ancor
+	/* era pra retirar ancor e colocar no final	-------------------------------------------------------
+	-DUVIDA: ele insere porque o tamanho aumenta, mas o objeto fica indefinido,
+	 AFETANDO next(ABAIXO).
+	*/
+	position.push(ancor); // pra fechar o ciclo 	
+	position.splice(ind, 1); 
+	
+	// j=1
+	hull.push(position[0].point); // para inicializar topo e não dar estouro na pilha
 	
 	var prev = hull[0],
+	//	topo,
 		topo = hull[1],
 		next,
-		giroEsq = false,
+		giroEsq,
 		x1, x2, x3, y1, y2, y3,
-		j = 2;
-	
-	// ainda vou ver a quantidade de loops
-	// ainda vou ver a logica ...
-	// DUVIDA: O PRODUTO VETORIAL ESTÁ DANDO NEGATIVO PARA PONTOS QUE EU SEI Q SERIA POSITIVO??????????????????
-	for (var i=1; i<position.length; i++) {		
-		next = position[i].point;
-				
+		j = 1; 
+			
+	for (var i=1; i<position.length; i++) {
+		
+		// isso é porque não estou conseguindo inserir "position.push(ancor); // pra fechar o ciclo"
+		if (i!=4){
+			next = position[i].point; // a conta seria apenas essa
+		} else {
+			next = points[0];
+		}
+			
 		x1 = prev.x;
 		x2 = topo.x;
 		x3 = next.x;
 		y1 = prev.y;
 		y2 = topo.y;
 		y3 = next.y;
-		
+							
 		prodVetorial(x1, x2, x3, y1, y2, y3);
 		
 		if(giroEsq) {
-			//o indice de hull é sempre um a mais que position:
-			// hull[i+1] = position[i].point;
-			hull.push(next);	//ainda vou ver quando tiro ou boto em hull	
+			
+			hull.push(next);
+			j++;			
 			prev = hull[j-1];
 			topo = hull[j];
-			j++;
+			
 		} else {
-			topo = next;
+			hull.pop(); // retira topo da pilha
+			j--;
+			prev = hull[j-1];
+			topo = hull[j];
+			i--;
 		}
-					
+						
 		console.log(giroEsq);
+			
 	}
-		
+
+//	console.log(hull);
+	
 	// calcula o produto vetorial para determinar se o o giro foi a esquerda ou direita
 function prodVetorial( x1, x2, x3, y1, y2, y3) {
+	giroEsq = false;
 	var prod;
-    prod = (x2-x1)*(y3-y1)-(y2-y1)*(x3-x1);
-	console.log(prod);
+	prod = (x2-x1)*(y3-y1)-(y2-y1)*(x3-x1);
+
     if (prod > 0){
 		giroEsq = true;
 	}	
  }
+	
