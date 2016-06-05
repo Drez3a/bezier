20986863c09a5f2d3e3333727b2ae0b59a7c4e4d
Drez3 <drezfabiola@gmail.com>
2016-06-05 02:57:33 -0300

    Update bezier.js

--- scripts/bezier.js
+++ scripts/bezier.js
@@ -165,91 +165,89 @@ var bezierConvex = (function() {
 	document.body.addEventListener('touchmove', static, false);	
 
 	var convexHull = function() {
-		var ind = 0,
-			position = [],
-			ancor = points[0];
+		var	ind = 0,
+			position = [],			
+			ancor = points[0],
+			ext = [],
 			hull = [];
-
+		
+		/*points = [{x: 57, y: 198}, {x: 217, y: 83}, {x: 446, y: 230}, {x: 248, y: 202}, {x:
+			339, y: 332}, {x: 568, y: 321}, {x: 559, y: 160}, {x: 270, y: 435}, {x: 136, y: 402}, {x:
+			22, y: 324}, {x: 105, y: 60}, {x: 60, y: 60}],*/
+				
 		if (points.length < 3) return;
-
+		
 		for (var i=0; i<points.length; i++){
 			position[i] = {
-				// os valores dos angulos estão estranhos
+				// os valores dos ângulos estão estranhos:
+				// o menor ângulo, por atan2, fica com maior valor
 				angle: Math.atan2(points[i].x, points[i].y),
 				point: points[i]
-			}
-
-			if(ancor.y > points[i].y) {
-				ind = i;
-				ancor = points[i];
-			}
 		}
-
-		position.sort(function(prev, next) {
-		 return next.angle - prev.angle;
-		});
-
-		//hull.push(ancor); // j = 0
-
-		/* era pra retirar ancor e colocar no final	-------------------------------------------------------
-		 -DUVIDA: ele insere porque o tamanho aumenta, mas o objeto fica indefinido,
-		 AFETANDO next(ABAIXO).
-		 */
-		position.push(ancor); // pra fechar o ciclo
-		position.splice(ind, 1);
-
-		// j=1
-		hull.push(position[0].point); // para inicializar topo e não dar estouro na pilha
-
-		var prev = hull[0],
-		//	topo,
-			topo = hull[1],
-			next,
-			giroEsq,
-			x1, x2, x3, y1, y2, y3,
-			j = 1;
-
-		for (var i=1; i<position.length; i++) {
-
-			// isso é porque não estou conseguindo inserir "position.push(ancor); // pra fechar o ciclo"
-			if (i!=4){
-				next = position[i].point; // a conta seria apenas essa
-			} else {
-				next = points[0];
-			}
-
-			x1 = prev.x;
-			x2 = topo.x;
-			x3 = next.x;
-			y1 = prev.y;
-			y2 = topo.y;
-			y3 = next.y;
-
-			prodVetorial(x1, x2, x3, y1, y2, y3);
-
-			if(giroEsq) {
-
-				hull.push(next);
-				j++;
-				prev = hull[j-1];
-				topo = hull[j];
-
-			} else {
-				hull.pop(); // retira topo da pilha
-				j--;
-				prev = hull[j-1];
-				topo = hull[j];
-				i--;
-			}
-
-			console.log(giroEsq);
-
+		if(ancor.y > position[i].point.y) {
+			ind = i;
+			ancor = position[i].point;			
 		}
+	}
+	
+	// guardando a posição do ponto mais extremo para fechar o poligono
+	ext = position[ind];	
+	
+	// Retirar o ponto mais extremo de position
+	position.splice(ind, 1); 
+		
+	position.sort(function(prev, next) {
+		return prev.angle - next.angle; 
+	});
+	
+	// porque os resultados dos ângulos estão dando ao contrario do que deveria ser
+	position.reverse();
+	
+	// ponto mais extremo para fechar o poligono
+	position.push(ext); 	
+		
+	// inicializando hull com os dois primeiros elementos: j = 1
+	// a cada iteração são analisados dois pontos na pilha com 
+	hull.push(ancor); 	
+	hull.push(position[0].point); 
+	
+	var prev = hull[0],	
+		topo = hull[1],
+		next,
+		giroEsq,
+		x1, x2, x3, y1, y2, y3,
+		j = 1; 
+				
+	for (var i=1; i<position.length; i++) {
+				
+		next = position[i].point; 
+						
+		x1 = prev.x;
+		x2 = topo.x;
+		x3 = next.x;
+		y1 = prev.y;
+		y2 = topo.y;
+		y3 = next.y;
+							
+		prodVetorial(x1, x2, x3, y1, y2, y3);
+		
+		// topo é quem esta sendo testado 
+		if(giroEsq) {			
+			prev = topo;	
+		} else {			
+			hull.pop(); // retira topo da pilha		
+		}
+		
+		topo = next;
+		hull.push(next);			
+	}
+	console.log("hull final:");
+	console.log(hull);
 
-		// calcula o produto vetorial para determinar se o o giro foi a esquerda ou direita
-		function prodVetorial( x1, x2, x3, y1, y2, y3) {
-			giroEsq = false;
-			var prod;
+	// calcula o produto vetorial para determinar se o o giro foi a esquerda ou direita
+	function prodVetorial( x1, x2, x3, y1, y2, y3) {
+		giroEsq = false;
+		var prod;
 			prod = (x2-x1)*(y3-y1)-(y2-y1)*(x3-x1);
 
 			if (prod > 0){
